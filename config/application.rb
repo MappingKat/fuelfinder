ad549d81c283f35f1cc90f38845090d270f037f4
Katrina Engelsted <katrina@engelsted.co>
2014-02-27 13:49:48 -0700

    changing settings

--- config/application.rb
+++ config/application.rb
@@ -3,10 +3,10 @@ require File.expand_path('../boot', __FILE__)
 # Pick the frameworks you want:
 # require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
+# require "action_mailer/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-require 'dotenv' ; Dotenv.load ".env.local", ".env.#{Rails.env}"
+# require 'dotenv' ; Dotenv.load ".env.local", ".env.#{Rails.env}"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
