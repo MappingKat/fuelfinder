5ff9debb14afcaa2728dfb658c721f4f9fe7e0f4
Katrina Engelsted <katrina@engelsted.co>
2014-02-27 13:43:32 -0700

    removing reviews

--- app/controllers/reviews_controller.rb
+++
@@ -1,7 +0,0 @@
-class ReviewsController < ApplicationController
-  def create
-    @review = Review.new(params).save
-    station = params[:station_id]
-    redirect_to station_path(station)
-  end
-end
